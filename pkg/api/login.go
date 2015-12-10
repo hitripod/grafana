64b544d74bf05eadeeed8240ffee6ad8a0d4943f
Don Hsieh <don@cepave.com>
2015-12-10 14:40:22 +0800

    [OWL-201] move func getOpenFalconSessionUsername() to pkg/middleware/auth.go

--- pkg/api/login.go
+++ pkg/api/login.go
@@ -1,12 +1,7 @@
 package api
 
 import (
-	"database/sql"
-	_ "github.com/go-sql-driver/mysql"
-	l "log"
 	"net/url"
-	"strconv"
-	"time"
 
 	"github.com/Cepave/grafana/pkg/api/dtos"
 	"github.com/Cepave/grafana/pkg/bus"
@@ -23,121 +18,14 @@ const (
 	VIEW_INDEX = "index"
 )
 
-/**
- * @function name:	GetOpenFalconSessionUsername(sig string) string
- * @description:	This function gets user logged in if "sig" cookie of Open-Falcon is valid.
- * @related issues:	OWL-159, OWL-124, OWL-115, OWL-110
- * @param:			sig string
- * @return:			username string
- * @author:			Don Hsieh
- * @since:			10/07/2015
- * @last modified: 	11/30/2015
- * @called by:		func LoginWithOpenFalconCookie(c *middleware.Context) bool
- *					 in pkg/api/login.go
- */
-func GetOpenFalconSessionUsername(sig string) string {
-	if sig == "" {
-		return ""
-	}
-
-	str := configOpenFalcon.Db.Addr
-	db, err := sql.Open("mysql", str)
-	db.SetMaxOpenConns(2000)
-	db.SetMaxIdleConns(1000)
-	defer db.Close()
-
-	if err != nil {
-		return ""
-	}
-	
-	stmtOut, err := db.Prepare("SELECT id, uid, expired FROM uic.session WHERE sig = ?")
-	if err != nil {
-		l.Println(err.Error())
-		return ""
-	}
-	defer stmtOut.Close()
-
-	var id int64
-	var uid int64
-	var expired string
-	err = stmtOut.QueryRow(sig).Scan(&id, &uid, &expired) // WHERE id = endpointId
-	if err != nil {
-		l.Println(err.Error())
-		return ""
-	}
-	l.Println("Expired time stamp =", expired)
-
-	expiredTimeInt, err := strconv.ParseInt(expired, 10, 64)
-	if err != nil {
-		l.Println(err.Error())
-		return ""
-	}
-
-	now := time.Now().Unix()
-	l.Println("now =", now)
-	isExpired := now > expiredTimeInt
-	l.Println("isExpired =", isExpired)
-	if isExpired {
-		return ""
-	}
-
-	stmtOut, err = db.Prepare("SELECT name FROM uic.user WHERE id = ?")
-	if err != nil {
-		l.Println(err.Error())
-		return ""
-	}
-	defer stmtOut.Close()
-
-	var name string
-	err = stmtOut.QueryRow(uid).Scan(&name) // WHERE id = endpointId
-	if err != nil {
-		l.Println(err.Error())
-		return ""
-	}
-	l.Println("Session user name =", name)
-	return name
-}
-
-/**
- * @function name:	func LoginWithOpenFalconCookie(c *middleware.Context) bool
- * @description:	This function gets user logged in if "sig" cookie of Open-Falcon is valid.
- * @related issues:	OWL-115, OWL-110
- * @param:			c *middleware.Context
- * @return:			bool
- * @author:			Don Hsieh
- * @since:			10/06/2015
- * @last modified: 	10/07/2015
- * @called by:		func LoginView(c *middleware.Context)
- *					 in pkg/api/login.go
- */
-func LoginWithOpenFalconCookie(c *middleware.Context) bool {
-	sig := c.GetCookie("sig")
-	uname := GetOpenFalconSessionUsername(sig)
-	if uname == "" {
-		return false
-	}
-	
-	userQuery := m.GetUserByLoginQuery{LoginOrEmail: uname}
-	if err := bus.Dispatch(&userQuery); err == nil {
-		user := userQuery.Result
-		loginUserWithUser(user, c)
-		return true
-	}
-
-	uname = "admin"
-	userQuery = m.GetUserByLoginQuery{LoginOrEmail: uname}
-	if err := bus.Dispatch(&userQuery); err == nil {
-		user := userQuery.Result
-		loginUserWithUser(user, c)
-		return true
-	}
-	return false
-}
 
 func LoginView(c *middleware.Context) {
-	isLoggedIn := LoginWithOpenFalconCookie(c)
-	if isLoggedIn {
+	sig := c.GetCookie("sig")
+	if len(sig) > 0 {
 		c.Redirect(setting.AppSubUrl + "/")
+		return
+	} else {
+		c.Redirect(setting.ConfigOpenFalcon.Login)
 		return
 	}
 
@@ -256,6 +144,7 @@ func loginUserWithUser(user *m.User, c *middleware.Context) {
 func Logout(c *middleware.Context) {
 	c.SetCookie(setting.CookieUserName, "", -1, setting.AppSubUrl+"/")
 	c.SetCookie(setting.CookieRememberName, "", -1, setting.AppSubUrl+"/")
+	c.SetCookie("sig", "", -1, setting.AppSubUrl+"/")
 	c.Session.Destory(c)
-	c.Redirect(setting.AppSubUrl + "/login")
+	c.Redirect(setting.ConfigOpenFalcon.Login)
 }
--- pkg/middleware/auth.go
+++ pkg/middleware/auth.go
@@ -1,6 +1,14 @@
 package middleware
 
 import (
+	"database/sql"
+	"github.com/Cepave/grafana/pkg/bus"
+	"github.com/Cepave/grafana/pkg/util"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+	"strconv"
+	"time"
+
 	"net/url"
 	"strings"
 
@@ -71,8 +79,135 @@ func RoleAuth(roles ...m.RoleType) macaron.Handler {
 	}
 }
 
+/**
+ * @function name:   getOpenFalconSessionUsername(sig string) string
+ * @description:     This function returns username if "sig" cookie of Open-Falcon is valid.
+ * @related issues:  OWL-201, OWL-159, OWL-124, OWL-115, OWL-110
+ * @param:           sig string
+ * @return:          username string
+ * @author:          Don Hsieh
+ * @since:           10/07/2015
+ * @last modified:   12/09/2015
+ * @called by:       func Auth(options *AuthOptions) macaron.Handler
+ *                    in pkg/middleware/auth.go
+ */
+func getOpenFalconSessionUsername(sig string) string {
+	if sig == "" {
+		return ""
+	}
+
+	str := setting.ConfigOpenFalcon.Db.Addr
+	db, err := sql.Open("mysql", str)
+	db.SetMaxOpenConns(2000)
+	db.SetMaxIdleConns(1000)
+	defer db.Close()
+
+	if err != nil {
+		return ""
+	}
+
+	stmtOut, err := db.Prepare("SELECT id, uid, expired FROM uic.session WHERE sig = ?")
+	if err != nil {
+		log.Println(err.Error())
+		return ""
+	}
+	defer stmtOut.Close()
+
+	var id int64
+	var uid int64
+	var expired string
+	err = stmtOut.QueryRow(sig).Scan(&id, &uid, &expired) // WHERE id = endpointId
+	if err != nil {
+		log.Println(err.Error())
+		return ""
+	}
+	expiredTimeInt, err := strconv.ParseInt(expired, 10, 64)
+	if err != nil {
+		log.Println(err.Error())
+		return ""
+	}
+
+	now := time.Now().Unix()
+	isExpired := now > expiredTimeInt
+	if isExpired {
+		return ""
+	}
+
+	stmtOut, err = db.Prepare("SELECT name FROM uic.user WHERE id = ?")
+	if err != nil {
+		log.Println(err.Error())
+		return ""
+	}
+	defer stmtOut.Close()
+
+	var name string
+	err = stmtOut.QueryRow(uid).Scan(&name) // WHERE id = endpointId
+	if err != nil {
+		log.Println(err.Error())
+		return ""
+	}
+	return name
+}
+
+func loginUserWithUser(user *m.User, c *Context) {
+	if user == nil {
+		log.Println(3, "User login with nil user")
+	}
+
+	days := 86400 * setting.LogInRememberDays
+	c.SetCookie(setting.CookieUserName, user.Login, days, setting.AppSubUrl+"/")
+	c.SetSuperSecureCookie(util.EncodeMd5(user.Rands+user.Password), setting.CookieRememberName, user.Login, days, setting.AppSubUrl+"/")
+
+	c.Session.Set(SESS_KEY_USERID, user.Id)
+}
+
+/**
+ * @function name:   func loginWithOpenFalconCookie(c *Context, username string)
+ * @description:     This function gets user logged in if "sig" cookie of Open-Falcon is valid.
+ * @related issues:  OWL-201, OWL-115, OWL-110
+ * @param:           c *middleware.Context
+ * @param:           username string
+ * @return:          void
+ * @author:          Don Hsieh
+ * @since:           10/06/2015
+ * @last modified:   12/09/2015
+ * @called by:       func Auth(options *AuthOptions) macaron.Handler
+ *                    in pkg/middleware/auth.go
+ */
+func loginWithOpenFalconCookie(c *Context, username string) {
+	userQuery := m.GetUserByLoginQuery{LoginOrEmail: username}
+	err := bus.Dispatch(&userQuery)
+	if err == nil {
+		user := userQuery.Result
+		loginUserWithUser(user, c)
+	} else {
+		username = "admin"
+		userQuery = m.GetUserByLoginQuery{LoginOrEmail: username}
+		err := bus.Dispatch(&userQuery)
+		if err == nil {
+			user := userQuery.Result
+			loginUserWithUser(user, c)
+		} else {
+			log.Println("Error =", err.Error())
+		}
+	}
+}
+
 func Auth(options *AuthOptions) macaron.Handler {
 	return func(c *Context) {
+		sig := c.GetCookie("sig")
+		if len(sig) == 0  && options.ReqSignedIn && !c.AllowAnonymous {
+			c.SetCookie(setting.CookieUserName, "", -1, setting.AppSubUrl+"/")
+			c.SetCookie(setting.CookieRememberName, "", -1, setting.AppSubUrl+"/")
+			c.Session.Destory(c)
+			c.Redirect(setting.ConfigOpenFalcon.Login)
+			return
+		}
+		if !c.IsSignedIn {
+			username := getOpenFalconSessionUsername(sig)
+			loginWithOpenFalconCookie(c, username)
+		}
+
 		if !c.IsSignedIn && options.ReqSignedIn && !c.AllowAnonymous {
 			notAuthorized(c)
 			return
